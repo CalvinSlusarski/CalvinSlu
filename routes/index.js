43f543bd2899cf9c34fd42af7f6d81ba98b64d30
Calvin <smackcalvin@gmail.com>
2015-04-08 14:21:46 -0500

    odd happenings

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.sendfile('unity.html');
+  res.sendfile('index.html');
 });
 
 module.exports = router;
