06cbe82e24f213a3c8be5b3e015c8aa94f7287ab
Calvin <smackcalvin@gmail.com>
2015-04-08 13:39:01 -0500

    added unity demo
    and routing.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.sendfile('index.html');
+  res.sendfile('main/index.html');
 });
 
 module.exports = router;
