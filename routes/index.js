cc3eb04a24d763593c3f085d29bbc5c8d3b65ef7
Calvin <smackcalvin@gmail.com>
2015-04-08 13:54:03 -0500

    yaa

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.sendfile(path.resolve('public/main/index.html'));
+  res.sendfile(__dirname + '/main/index.html');
 });
 
 module.exports = router;
