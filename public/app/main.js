6aa5d0d9410d93ae40b16db585c370534a977505
calvinslusarski <smackcalvin@gmail.com>
2015-04-11 15:22:40 -0500

    no idea

--- public/app/main.js
+++ public/app/main.js
@@ -6,14 +6,14 @@
         'transitions' : '../lib/durandal/js/transitions',
         'knockout': '../lib/knockout/knockout-3.1.0',
         'bootstrap': '../lib/bootstrap/js/bootstrap',
-        // 'jquery': '../lib/jquery/jquery-1.9.1'
-    }//,
-    // shim: {
-    //     'bootstrap': {
-    //         deps: ['jquery'],
-    //         exports: 'jQuery'
-    //     }
-    // }
+        'jquery': '../lib/jquery/jquery-1.9.1'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+        }
+    }
 });
 
 define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
