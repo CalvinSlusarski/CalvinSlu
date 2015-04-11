0526429ff8f916d87095f20cefca5612663fb82d
calvinslusarski <smackcalvin@gmail.com>
2015-04-11 15:57:33 -0500

    removing stupid widget

--- public/app/widgets/expander/viewmodel.js
+++ public/app/widgets/expander/viewmodel.js
@@ -1,4 +1,4 @@
-﻿define(['durandal/composition','jquery'], function(composition, $) {
+﻿define(['durandal/composition'], function(composition) {
     var ctor = function() { };
 
     ctor.prototype.activate = function(settings) {
