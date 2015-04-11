0b9cd15487f7420e4693a308e7a03b36dd6e4708
calvinslusarski <smackcalvin@gmail.com>
2015-04-11 10:54:20 -0500

    using knockout to generate photos

--- public/app/photobook/index.js
+++ public/app/photobook/index.js
@@ -1,34 +1,53 @@
 ﻿define(['durandal/app', 'durandal/system', 'knockout'], function (app, system, ko) {
-    var name = ko.observable();
-    var canSayHello = ko.computed(function () {
-        return name() ? true : false;
-    });
+    var vm = {};
+    vm.photoInArray = ko.observableArray([]);
+    // temp
+    vm.photoInArray([
+        new photo(
+            'https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/1935715_1247480994083_1588857_n.jpg?oh=b3aaa25ff227eaed78d559786b40701d&oe=55E14ECE&__gda__=1435988421_006f033de281db2cb6af7e0f05d1726c'
+        )]),
+        new photo(
+            'https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/1935715_1247480994083_1588857_n.jpg?oh=b3aaa25ff227eaed78d559786b40701d&oe=55E14ECE&__gda__=1435988421_006f033de281db2cb6af7e0f05d1726c'
+        )]),
+    // end of temp
 
-    return {
-        displayName: 'What is your name?',
-        name: name,
-        sayHello: function() {
-            app.showMessage('Hello ' + name() + '!', 'Greetings');
-        },
-        canSayHello: canSayHello,
-        activate: function() {
-            system.log('Lifecycle : activate : hello');
-        },
-        binding: function () {
-            system.log('Lifecycle : binding : hello');
-            return { cacheViews:false }; //cancels view caching for this module, allowing the triggering of the detached callback
-        },
-        bindingComplete: function () {
-            system.log('Lifecycle : bindingComplete : hello');
-        },
-        attached: function (view, parent) {
-            system.log('Lifecycle : attached : hello');
-        },
-        compositionComplete: function (view) {
-            system.log('Lifecycle : compositionComplete : hello');
-        },
-        detached: function (view) {
-            system.log('Lifecycle : detached : hello');
-        }
-    };
+    return vm;
+
+    // var name = ko.observable();
+    // var canSayHello = ko.computed(function () {
+    //     return name() ? true : false;
+    // });
+
+    // return {
+    //     displayName: 'What is your name?',
+    //     name: name,
+    //     sayHello: function() {
+    //         app.showMessage('Hello ' + name() + '!', 'Greetings');
+    //     },
+    //     canSayHello: canSayHello,
+    //     activate: function() {
+    //         system.log('Lifecycle : activate : hello');
+    //     },
+    //     binding: function () {
+    //         system.log('Lifecycle : binding : hello');
+    //         return { cacheViews:false }; //cancels view caching for this module, allowing the triggering of the detached callback
+    //     },
+    //     bindingComplete: function () {
+    //         system.log('Lifecycle : bindingComplete : hello');
+    //     },
+    //     attached: function (view, parent) {
+    //         system.log('Lifecycle : attached : hello');
+    //     },
+    //     compositionComplete: function (view) {
+    //         system.log('Lifecycle : compositionComplete : hello');
+    //     },
+    //     detached: function (view) {
+    //         system.log('Lifecycle : detached : hello');
+    //     }
+    // };
+    function photo(url, comment){
+        self = this;
+        self.url = url;
+        self.comment = comment;
+    }
 });
