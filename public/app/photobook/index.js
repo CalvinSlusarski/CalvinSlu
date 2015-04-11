713b1dc10c3de13806eab2d07b162ddce9c03c12
calvinslusarski <smackcalvin@gmail.com>
2015-04-11 13:35:26 -0500

    adding css and moving stuff around.

--- public/app/photobook/index.js
+++ public/app/photobook/index.js
@@ -7,8 +7,9 @@
             'https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/1935715_1247480994083_1588857_n.jpg?oh=b3aaa25ff227eaed78d559786b40701d&oe=55E14ECE&__gda__=1435988421_006f033de281db2cb6af7e0f05d1726c'
         ),
         new photo(
-            'https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/1935715_1247480994083_1588857_n.jpg?oh=b3aaa25ff227eaed78d559786b40701d&oe=55E14ECE&__gda__=1435988421_006f033de281db2cb6af7e0f05d1726c'
+            'https://scontent-ord.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/4261_1145626807792_8143272_n.jpg?oh=29227312bf3639556270e2a55883b67c&oe=55E074FE'
         )]);
+        
     // end of temp
 
     return vm;
