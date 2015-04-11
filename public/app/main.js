07a6ab0486cea6d1f2dbca00e1b8a1321b70ce1e
calvinslusarski <smackcalvin@gmail.com>
2015-04-11 16:14:27 -0500

    typo fix

--- public/app/main.js
+++ public/app/main.js
@@ -5,7 +5,7 @@
         'plugins' : '../lib/durandal/js/plugins',
         'transitions' : '../lib/durandal/js/transitions',
         'knockout': '../lib/knockout/knockout-3.1.0',
-        'bootstrap': '../lib/bootstrap/js/bootstrap'
+        'bootstrap': '../lib/bootstrap/js/bootstrap',
         'jquery': '../lib/jquery/jquery-1.9.1',
         'jquery-ui': '../lib/jquery-ui/jquery-ui.min.js'
     },
